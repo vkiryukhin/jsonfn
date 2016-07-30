299b4beb4818f7ca98f5e01f0d2a6675b8b2e4ac
Kiryukhin, Vadim <vkiryukhin@conversantmedia.com>
2016-07-30 01:14:45 -0700

    code cleanup

--- jsonfn.js
+++ jsonfn.js
@@ -34,16 +34,15 @@
   exports.stringify = function (obj) {
 
     return JSON.stringify(obj, function (key, value) {
-
       var fnBody;
       if (value instanceof Function || typeof value == 'function') {
+
 
         fnBody = value.toString();
 
         if (fnBody.length < 8 || fnBody.substring(0, 8) !== 'function') { //this is ES6 Arrow Function
           return '_NuFrRa_' + fnBody;
         }
-
         return fnBody;
       }
       if (value instanceof RegExp) {
