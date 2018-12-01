076a57b891603ad6e93e038204609b608735213a
Anh <anhoev@yahoo.de>
2018-12-01 15:18:51 +0100

    add mongoose schema stringify

--- jsonfn.js
+++ jsonfn.js
@@ -29,66 +29,79 @@
 */
 
 (function (exports) {
-"use strict";
+   "use strict";
 
-  exports.stringify = function (obj) {
+   exports.stringify = function (obj) {
 
-    return JSON.stringify(obj, function (key, value) {
-      var fnBody;
-      if (value instanceof Function || typeof value == 'function') {
+      return JSON.stringify(obj, function (key, value) {
+         let fnBody;
+         if (value === String) return '_Schema_String';
+         else if (value === Number) return '_Schema_Number';
+         else if (value === Boolean) return '_Schema_Boolean';
+         else if (value === Date) return '_Schema_Date';
+         else if (value === Buffer) return '_Schema_Buffer';
+         else if (value === Array) return '_Schema_Array';
+         else if (value === Map) return '_Schema_Map';
 
+         if (value instanceof Function || typeof value == 'function') {
+            fnBody = value.toString();
 
-        fnBody = value.toString();
+            if (fnBody.length < 8 || fnBody.substring(0, 8) !== 'function') { //this is ES6 Arrow Function
+               return '_NuFrRa_' + fnBody;
+            }
+            return fnBody;
+         }
+         if (value instanceof RegExp) {
+            return '_PxEgEr_' + value;
+         }
+         return value;
+      });
+   };
 
-        if (fnBody.length < 8 || fnBody.substring(0, 8) !== 'function') { //this is ES6 Arrow Function
-          return '_NuFrRa_' + fnBody;
-        }
-        return fnBody;
-      }
-      if (value instanceof RegExp) {
-        return '_PxEgEr_' + value;
-      }
-      return value;
-    });
-  };
+   exports.parse = function (str, date2obj) {
 
-  exports.parse = function (str, date2obj) {
+      const iso8061 = date2obj ? /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/ : false;
 
-    var iso8061 = date2obj ? /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/ : false;
+      return JSON.parse(str, function (key, value) {
+         let prefix;
 
-    return JSON.parse(str, function (key, value) {
-      var prefix;
+         if (typeof value != 'string') {
+            return value;
+         }
+         if (value.length < 8) {
+            return value;
+         }
 
-      if (typeof value != 'string') {
-        return value;
-      }
-      if (value.length < 8) {
-        return value;
-      }
+         prefix = value.substring(0, 8);
 
-      prefix = value.substring(0, 8);
+         if (iso8061 && value.match(iso8061)) {
+            return new Date(value);
+         }
+         if (prefix === 'function') {
+            return eval('(' + value + ')');
+         }
+         if (prefix === '_PxEgEr_') {
+            return eval(value.slice(8));
+         }
+         if (prefix === '_NuFrRa_') {
+            return eval(value.slice(8));
+         }
+         if (prefix === '_Schema_') {
+            if (value === '_Schema_String') return String;
+            if (value === '_Schema_Number') return Number;
+            if (value === '_Schema_Boolean') return Boolean;
+            if (value === '_Schema_Date') return Date;
+            if (value === '_Schema_Buffer') return Buffer;
+            if (value === '_Schema_Array') return Array;
+            if (value === '_Schema_Map') return Map;
+         }
 
-      if (iso8061 && value.match(iso8061)) {
-        return new Date(value);
-      }
-      if (prefix === 'function') {
-        return eval('(' + value + ')');
-      }
-      if (prefix === '_PxEgEr_') {
-        return eval(value.slice(8));
-      }
-      if (prefix === '_NuFrRa_') {
-        return eval(value.slice(8));
-      }
+         return value;
+      });
+   };
 
-      return value;
-    });
-  };
-
-  exports.clone = function (obj, date2obj) {
-    return exports.parse(exports.stringify(obj), date2obj);
-  };
+   exports.clone = function (obj, date2obj) {
+      return exports.parse(exports.stringify(obj), date2obj);
+   };
 
 }(typeof exports === 'undefined' ? (window.JSONfn = {}) : exports));
-
-
