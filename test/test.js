73525259eea36f2bffc375aaf7e690051a4e9012
Anh Duc <anhduc.hoang@vmodev.com>
2019-01-25 13:27:17 +0700

    add test, code support

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const jsonFn = require('./jsonfn');
+
+const testObject = {
+  value: 1234,
+  string: '1234',
+  code: function(a) {
+    return a * 2;
+  }
+};
+
+const stringify = jsonFn.stringify(testObject, true);
+console.log(stringify);
+
+const parse = jsonFn.parse(stringify, null, true);
+console.log(parse);
